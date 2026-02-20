769eb014b94dadb5da542dcb0171d808b17453d3
harshitmlzs15-code <harshitmlzs15@gmail.com>
2026-02-20 19:58:15 +0530

    harshit

---
+++ comparision.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout<<" enter a: "<<endl;
+  cin>>a;
+   cout<<" enter b: "<<endl;
+  cin>>b;
+
+  if(a==b){
+    cout<<"1"<<endl;
+  }
+  else{
+    cout<<"0"<<endl;
+  }
+ return 0;
+}
